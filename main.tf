d2bb20468db40d27501c6cf13c1fc9a6e25d1ff5
tyagiabhay <abhaytyagi.m@gmail.com>
2022-09-26 14:45:04 +0530

    dynamic-bucket-name

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Creating bucket with dynamic name
 resource "aws_s3_bucket" "bucket" {
   bucket        = var.bucket_name
   force_destroy = true
@@ -6,39 +7,7 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "my-policy" {
-#   bucket = var.bucket_name
-#   policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Id" : "Policy1464968545158",
-#     "Statement" : [
-#       {
-#         "Sid" : "",
-#         "Effect" : "Allow",
-#         "Principal" : "*",
-#         "Action" : "s3:PutObject",
-#         "Resource" : [
-#           "arn:aws:s3:::${var.bucket_name}/*.jpg",
-#           "arn:aws:s3:::${var.bucket_name}/*.png",
-#           "arn:aws:s3:::${var.bucket_name}/*.gif"
-#         ]
-#       },
-#       {
-#         "Sid" : "",
-#         "Effect" : "Deny",
-#         "Principal" : "*",
-#         "Action" : "s3:PutObject",
-#         "NotResource" : [
-#           "arn:aws:s3:::${var.bucket_name}/*.jpg",
-#           "arn:aws:s3:::${var.bucket_name}/*.png",
-#           "arn:aws:s3:::${var.bucket_name}/*.gif"
-#         ]
-#       }
-#     ]
-#   })
-
-# }
-
+# Adding policy to restrict file uploads for only .png, .jpg, .gif extensions. 
 resource "aws_s3_bucket_policy" "my-policy" {
   bucket = var.bucket_name
   policy = jsonencode(
